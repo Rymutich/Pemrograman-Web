4958a4ad663099c6491dfa186800264db5989072
Rymutich <rizqifahmi999@gmail.com>
2022-12-20 10:15:25 +0700

    first Github Project

---
+++ app.py
@@ -0,0 +1,8 @@
+#hitung luas segitiga
+def luas_segitiga():
+    a=int (input("masukkan alas segitiga"))
+    t=int (input("masukkan tinggi segitiga"))
+    luas=a*t/2
+    print ("Luas sehitiga adalah :  ",luas)
+
+    luas_segitiga()
